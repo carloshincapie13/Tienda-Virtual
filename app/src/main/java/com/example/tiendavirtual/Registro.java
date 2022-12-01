e02d2baf011feaf6e2261d72ca93e6588c3cae5c
YALIRAv <113562786+YALIRAv@users.noreply.github.com>
2022-11-30 21:08:15 -0500

    Firebase
    Se realizó el primer paso de conexión con firebase

--- app/src/main/java/com/example/tiendavirtual/Registro.java
+++ app/src/main/java/com/example/tiendavirtual/Registro.java
@@ -1,37 +1,73 @@
 package com.example.tiendavirtual;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class Registro extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-    Button cancelar,crear;
+public class Registro extends AppCompatActivity {
+    private EditText et1Correo, et2ConfirmarCorreo, et3NombreUsuario, et4Password, et5ConfirmarPassword;
+    private FirebaseAuth firebaseAuth;
+
+    Button crear,cancelar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
-        crear = findViewById(R.id.btn8);
+        firebaseAuth =FirebaseAuth.getInstance();
+        et1Correo = findViewById(R.id.et1);
+        et2ConfirmarCorreo = findViewById(R.id.et2);
+        et3NombreUsuario = findViewById(R.id.et3);
+        et4Password = findViewById(R.id.et4);
+        et5ConfirmarPassword = findViewById(R.id.et5);
+
+        /*crear = findViewById(R.id.btn8);
         cancelar = findViewById(R.id.btn9);
-        cancelar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
+        cancelar.setOnClickListener(new View.OnClickListener() */
+
+
+
         crear.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                Toast.makeText(Registro.this, "Usuario Creado", Toast.LENGTH_SHORT).show();
-                Intent i = new Intent(Registro.this, Ingresar.class);
-                startActivity(i);
+                String correo = et1Correo.getText().toString();
+                String confirmarCorreo = et2ConfirmarCorreo.getText().toString();
+                String NombreUsuario = et3NombreUsuario.getText().toString();
+                String Password = et4Password.getText().toString();
+                String ConfirmarPasword = et4Password.getText().toString();
+
+                firebaseAuth.signInWithEmailAndPassword(correo, confirmarCorreo).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Registro.this, "Usuario creado", Toast.LENGTH_SHORT).show();
+
+                            Intent i = new Intent(Registro.this, Ingresar.class);
+                            startActivity(i);
+                        }
+                    }
+
+                });
+
             }
         });
     }
 }
+
+
+
+
+
